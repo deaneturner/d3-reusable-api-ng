46acff72ed030f8c6a674774f223883b878c7f4b
Deane Turner <deaneturner.com@gmail.com>
2022-10-11 13:45:57 -0400

    chart to lib

--- projects/archetypal-bar/src/app/app.component.ts
+++ projects/archetypal-bar/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import ArchetypalBarChart from '../../../shared/src/lib/charts/archetypal-bar-chart';
 
 
 @Component({
@@ -12,66 +13,6 @@ export class AppComponent implements OnInit {
   title = 'archetypal-bar';
 
   ngOnInit(): void {
-    var svg = d3.select("svg"),
-      margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = +svg.attr("width") - margin.left - margin.right,
-      height = +svg.attr("height") - margin.top - margin.bottom;
-
-    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-      y = d3.scaleLinear().rangeRound([height, 0]);
-
-    var g = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.tsv("assets/data.tsv")
-      .then(function(data) {
-        return data.map(function(d) {
-          // @ts-ignore
-          d.frequency = +d.frequency;
-          return d;
-        });
-      })
-      .then(function(data) {
-        // @ts-ignore
-        x.domain(data.map(function(d) { return d.letter; }));
-        // @ts-ignore
-        y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-
-        g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
-
-        g.append("g")
-          .attr("class", "axis axis--y")
-          .call(d3.axisLeft(y).ticks(10, "%"))
-          .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("text-anchor", "end")
-          .text("Frequency");
-
-        // @ts-ignore
-        // @ts-ignore
-        // @ts-ignore
-        // @ts-ignore
-        // @ts-ignore
-        g.selectAll(".bar")
-          .data(data)
-          .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", // @ts-ignore
-            function(d) {
-            return x(<string>d['letter']); })
-          .attr("y", function(d) { // @ts-ignore
-            return y(d.frequency); })
-          .attr("width", x.bandwidth())
-          .attr("height", function(d) { // @ts-ignore
-            return height - y(d.frequency); });
-      })
-      .catch(function(error) {
-        throw error;
-      });
+    ArchetypalBarChart();
   }
 }
